36514d797f59e96920e0ae6d14462a015133558b
aiya000 <aiya000.develop@gmail.com>
2025-03-01 03:16:32 +0900

    hotfix: define-options.sh

--- define-options.sh
+++ define-options.sh
@@ -9,11 +9,16 @@
 dir="$(cd -- "$(dirname -- "${BASH_SOURCE:-$0}")" && pwd || exit 1)"
 source "$dir/sources/define-alt-export.sh"
 
-define-alt-export BASH_TOYS_INTERACTIVE_FILTER peco # or fzf
-define-alt-export BASH_TOYS_DUSTBOX_DIR "$HOME/.backup/dustbox"
-define-alt-export BASH_TOYS_MUSIC_PLAYER vlc
-define-alt-export BASH_TOYS_POMODORO_NOTIFICATION_MUSIC "$dir/assets/notify.mp3"
-define-alt-export BASH_TOYS_BATCAT_OPTIONS '--number'
-define-alt-export BASH_TOYS_BOOKMARK_URLS
+# TODO: Temporary overwrite variables. Shoud be fixed by using define-alt-export
+unset BASH_TOYS_INTERACTIVE_FILTER || true
+unset BASH_TOYS_DUSTBOX_DIR || true
+unset BASH_TOYS_MUSIC_PLAYER || true
+unset BASH_TOYS_POMODORO_NOTIFICATION_MUSIC || true
+unset BASH_TOYS_BATCAT_OPTIONS || true
+export BASH_TOYS_INTERACTIVE_FILTER=peco
+export BASH_TOYS_DUSTBOX_DIR="$HOME/.backup/dustbox"
+export BASH_TOYS_MUSIC_PLAYER=vlc
+export BASH_TOYS_POMODORO_NOTIFICATION_MUSIC="$dir/assets/notify.mp3"
+export BASH_TOYS_BATCAT_OPTIONS=--number
 
 export BASH_TOYS_LOADED_DEFAULT_OPTIONS=true
