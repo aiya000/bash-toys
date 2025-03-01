2215bcf66d6db678d6bc5fdfeef4efc8e6827744
aiya000 <aiya000.develop@gmail.com>
2025-03-02 00:35:37 +0900

    Allow to load define-options.sh two or more times

--- define-options.sh
+++ define-options.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# Defines default options for bash-toys commands.
+#
 # ```shell-session
 # $ source path/to/bash-toys/define-options.sh
 # ```
-
-[[ -n $BASH_TOYS_LOADED_DEFAULT_OPTIONS ]] && return
 
 dir="$(cd -- "$(dirname -- "${BASH_SOURCE:-$0}")" && pwd || exit 1)"
 
@@ -15,5 +15,3 @@ dir="$(cd -- "$(dirname -- "${BASH_SOURCE:-$0}")" && pwd || exit 1)"
 [[ -z $BASH_TOYS_POMODORO_NOTIFICATION_MUSIC ]] && export BASH_TOYS_POMODORO_NOTIFICATION_MUSIC="$dir/assets/notify.mp3"
 [[ -z $BASH_TOYS_POMODORO_NOTIFICATION_DURATION ]] && export BASH_TOYS_POMODORO_NOTIFICATION_DURATION=5
 [[ -z $BASH_TOYS_BATCAT_OPTIONS ]] && export BASH_TOYS_BATCAT_OPTIONS=--number
-
-export BASH_TOYS_LOADED_DEFAULT_OPTIONS=true
