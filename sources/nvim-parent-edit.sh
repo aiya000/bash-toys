6a326d339e81fc49117d4fa33a1d59b3f954ab25
aiya000 <aiya000.develop@gmail.com>
2025-10-16 05:38:55 +0900

    feat(sources): Create nvim-parent-edit.sh

---
+++ sources/nvim-parent-edit.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Neovim parent file opening via RPC
+# Usage:
+#
+# 1. Install this script & source this. Read ../README.md how to install
+# ```bash
+# # If standalone installed
+# source ~/bash-toys/sources/nvim-parent-edit.sh
+# ```
+#
+# 2. Start Neovim & Run below lua script to open your shell
+# ```lua
+# vim.fn.jobstart(vim.env.SHELL, {
+#   env = {
+#     NVIM_PARENT_ADDRESS = vim.v.servername,
+#   },
+# })
+# ```
+#
+# 3. Run `$ nvim somefile` in the child terminal (started in 2.) simply
+#   - Then, `somefile` is opened in your parent Neovim's new tab
+
+if [[ $NVIM_PARENT_ADDRESS == '' ]] ; then
+  return 1
+fi
+
+function nvim_parent_edit() {
+  if [[ $# -eq 0 ]] ; then
+    echo 'Usage: nvim <filename>'
+    return 1
+  fi
+
+  if [[ $NVIM_PARENT_ADDRESS == '' ]] ; then
+    echo 'NVIM_PARENT_ADDRESS is not set. '
+    return 1
+  fi
+
+  # Get the absolute file path
+  local filepath="$1"
+  if [[ "$filepath" != /* ]] ; then
+    filepath="$(realpath "$filepath")"
+  fi
+
+  # Open file in parent Neovim
+  if command nvim --server "$NVIM_PARENT_ADDRESS" --remote-tab "$filepath" 2>/dev/null ; then
+    echo "✓ Opened '$filepath' in parent Neovim"
+    return
+  fi
+
+  echo "✗ Failed to open file in parent Neovim"
+  return 1
+}
+
+alias nvim='nvim_parent_edit'
+echo "✓ Parent Neovim RPC integration ready (server: $NVIM_PARENT_ADDRESS)"
+
+# https://github.com/aiya000/bash-toys
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2025- aiya000
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
