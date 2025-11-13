c67a68764cf6a4b37769a5aa6f53da3dc8594c3e
aiya000 <aiya000.develop@gmail.com>
2025-11-13 20:56:46 +0900

    fix!: Change specification for pomodoro-timer notification when finished & Fix pomodoro-timer working on macOS

--- define-options.sh
+++ define-options.sh
@@ -11,10 +11,8 @@ dir="$(cd -- "$(dirname -- "${BASH_SOURCE:-$0}")" && pwd || exit 1)"
 # Set default values only if variables are not already set
 [[ -z $BASH_TOYS_INTERACTIVE_FILTER ]] && export BASH_TOYS_INTERACTIVE_FILTER=peco
 [[ -z $BASH_TOYS_DUSTBOX_DIR ]] && export BASH_TOYS_DUSTBOX_DIR="$HOME/.backup/dustbox"
-[[ -z $BASH_TOYS_MUSIC_PLAYER ]] && export BASH_TOYS_MUSIC_PLAYER=vlc
-[[ -z $BASH_TOYS_MUSIC_PLAYER_OPTIONS ]] && export BASH_TOYS_MUSIC_PLAYER_OPTIONS='--gain=0.3'
-[[ -z $BASH_TOYS_POMODORO_NOTIFICATION_MUSIC ]] && export BASH_TOYS_POMODORO_NOTIFICATION_MUSIC="$dir/assets/notify.mp3"
-[[ -z $BASH_TOYS_POMODORO_NOTIFICATION_DURATION ]] && export BASH_TOYS_POMODORO_NOTIFICATION_DURATION=5
+[[ -z $BASH_TOYS_WHEN_POMODORO_TIMER_FINISHED ]] && export BASH_TOYS_WHEN_POMODORO_TIMER_FINISHED="vlc --gain=0.3 $dir/assets/notify.mp3"
+[[ -z $BASH_TOYS_DURATION_WHEN_POMODORO_TIMER_FINISHED ]] && export BASH_TOYS_DURATION_WHEN_POMODORO_TIMER_FINISHED=5
 [[ -z $BASH_TOYS_BATCAT_OPTIONS ]] && export BASH_TOYS_BATCAT_OPTIONS=--number
 [[ -z $BASH_TOYS_OPEN_BOOKMARK_OPENER ]] && export BASH_TOYS_OPEN_BOOKMARK_OPENER=xdg-open
 [[ -z $BASH_TOYS_BOOKMARK_OPEN_BOOKMARKS ]] && export BASH_TOYS_BOOKMARK_OPEN_BOOKMARKS='(Google=https://www.google.co.jp)|(GitHub=https://github.com)'
