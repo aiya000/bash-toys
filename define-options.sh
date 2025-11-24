0d0ed2b52a87a63861c4eb89f2122dcee8d0c760
aiya000 <aiya000.develop@gmail.com>
2025-11-25 01:26:19 +0900

    feat(bin > pomodoro-timer): Add configurable default interval via `BASH_TOYS_POMODORO_DEFAULT_INTERVAL`
    - Added BASH_TOYS_POMODORO_DEFAULT_INTERVAL environment variable support
    - Updated define-options.sh with new configuration option (default: 5 minutes)
    - Improved empty string check from -n to != ''

--- define-options.sh
+++ define-options.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Defines default options for bash-toys commands.
+# Defines default values only if variables are not already set>
+# See also ./README.md for details and usage.
 #
 # ```shell-session
 # $ source path/to/bash-toys/define-options.sh
@@ -8,10 +9,10 @@
 
 dir="$(cd -- "$(dirname -- "${BASH_SOURCE:-$0}")" && pwd || exit 1)"
 
-# Set default values only if variables are not already set
 [[ -z $BASH_TOYS_INTERACTIVE_FILTER ]] && export BASH_TOYS_INTERACTIVE_FILTER=peco
 [[ -z $BASH_TOYS_DUSTBOX_DIR ]] && export BASH_TOYS_DUSTBOX_DIR="$HOME/.backup/dustbox"
 [[ -z $BASH_TOYS_WHEN_POMODORO_TIMER_FINISHED ]] && export BASH_TOYS_WHEN_POMODORO_TIMER_FINISHED="vlc --gain=0.3 $dir/assets/notify.mp3"
+[[ -z $BASH_TOYS_POMODORO_DEFAULT_INTERVAL ]] && export BASH_TOYS_POMODORO_DEFAULT_INTERVAL=5  # When not specified minutes for pomodoro-timer command, like `$ pomodoro-timer`
 [[ -z $BASH_TOYS_DURATION_WHEN_POMODORO_TIMER_FINISHED ]] && export BASH_TOYS_DURATION_WHEN_POMODORO_TIMER_FINISHED=5
 [[ -z $BASH_TOYS_BATCAT_OPTIONS ]] && export BASH_TOYS_BATCAT_OPTIONS=--number
 [[ -z $BASH_TOYS_OPEN_BOOKMARK_OPENER ]] && export BASH_TOYS_OPEN_BOOKMARK_OPENER=xdg-open
