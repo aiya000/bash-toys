1941a7dc6875364181160b22a27db1f7a7d7f130
aiya000 <aiya000.develop@gmail.com>
2025-12-10 13:42:57 +0900

    fix(notify-at): Fix compatibility for macOS

--- define-options.sh
+++ define-options.sh
@@ -13,7 +13,7 @@ dir="$(cd -- "$(dirname -- "${BASH_SOURCE:-$0}")" && pwd || exit 1)"
 [[ -z $BASH_TOYS_DUSTBOX_DIR ]] && export BASH_TOYS_DUSTBOX_DIR="$HOME/.backup/dustbox"
 [[ -z $BASH_TOYS_WHEN_POMODORO_TIMER_FINISHED ]] && export BASH_TOYS_WHEN_POMODORO_TIMER_FINISHED="vlc --gain=0.3 $dir/assets/notification-impact.mp3"
 [[ -z $BASH_TOYS_POMODORO_DEFAULT_INTERVAL ]] && export BASH_TOYS_POMODORO_DEFAULT_INTERVAL=5  # When not specified minutes for pomodoro-timer command, like `$ pomodoro-timer`
-[[ -z $BASH_TOYS_NOTIFY_AT_DEFAULT_SOUND ]] && export BASH_TOYS_NOTIFY_AT_DEFAULT_SOUND="$dir/assets/notification.ogg"
+[[ -z $BASH_TOYS_NOTIFY_AT_DEFAULT_SOUND ]] && export BASH_TOYS_NOTIFY_AT_DEFAULT_SOUND="$dir/assets/notification-1.mp3"
 [[ -z $BASH_TOYS_DURATION_WHEN_POMODORO_TIMER_FINISHED ]] && export BASH_TOYS_DURATION_WHEN_POMODORO_TIMER_FINISHED=5
 [[ -z $BASH_TOYS_BATCAT_OPTIONS ]] && export BASH_TOYS_BATCAT_OPTIONS=--number
 [[ -z $BASH_TOYS_OPEN_BOOKMARK_OPENER ]] && export BASH_TOYS_OPEN_BOOKMARK_OPENER=xdg-open
